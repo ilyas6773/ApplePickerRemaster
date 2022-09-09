258812103739315a391fc8780f1027fc9c3a23df
ilyas6773 <mister.tolegenov@yandex.ru>
2022-09-09 21:13:47 +0600

    feat: all Apples that collided with Basket will disappear

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -28,4 +28,15 @@ public class Basket : MonoBehaviour
         pos.x = mousePos3D.x;
         this.transform.position = pos;
     }
+
+    //This method is called every time when another objects collides with Basket
+    void OnCollisionEnter( Collision coll ){
+        //coll.gameObjects returns reference to an object Basket collided with
+        GameObject collidedWith = coll.gameObject;
+
+        //check if the collided object is an Apple
+        if ( collidedWith.tag == "Apple"){
+            Destroy (collidedWith);
+        }
+    }
 }
