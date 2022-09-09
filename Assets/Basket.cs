c35b22a8b75e79b77dd35895b2a1f056505e3884
ilyas6773 <mister.tolegenov@yandex.ru>
2022-09-09 21:07:28 +0600

    feat: implemented control over Basket with a mouse. 2D coordinates of the mouse are transfromed into 3D coordinates of the game. Z coordinates are negative Z coordinates of the MainCamera. Basket follows X coordinates of the mouse

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -13,6 +13,19 @@ public class Basket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Recieve current coordinates of the mouse from Input
+        Vector3 mousePos2D = Input.mousePosition;
+
+        //Z coordinate of the camera determines how far is the 
+        //mouse pointer in 3D space
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        //Transform 2D position into 3D coordinates of the game
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        //Move Basket along X axis to the X coordinate of the mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
     }
 }
