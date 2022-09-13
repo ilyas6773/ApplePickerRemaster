5ec2c0170bb7de17f1c1b829ce5d137fec34d830
ilyas6773 <mister.tolegenov@yandex.ru>
2022-09-13 12:32:23 +0600

    feat: now high score is stored in a local storage

--- Assets/HighScore.cs
+++ Assets/HighScore.cs
@@ -19,5 +19,20 @@ public class HighScore : MonoBehaviour
     {
         TMP_Text gt = this.GetComponent<TMP_Text>();  
         gt.text = "High Score: " + score; 
+
+        //Update HighScore in PlayerPrefs if necessary
+        if(score > PlayerPrefs.GetInt("HighScore")){
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+    }
+
+    void Awake() {
+        // if HighScore value already exists in PlayerPrefs, read it
+        if (PlayerPrefs.HasKey("HighScore")){
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+
+        //Save the High Score to a local storage
+        PlayerPrefs.SetInt("HighScore", score);
     }
 }
